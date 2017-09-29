311acfd5be8461ef19bdfb880e24bc54a8692fdf
ying32 <ying_33@live.cn>
2017-09-30 01:56:36 +0800

    events.go中参数大小写更改

--- vcl/events.go
+++ vcl/events.go
@@ -1,43 +1,45 @@
 package vcl
 
+// TNotifyEvent
 type TNotifyEvent func(sender IObject)
 
-// TUpDown  TUDBtnType
-type TUDClickEvent func(Sender IObject, Button int32)
+// TUDClickEvent TUpDown  TUDBtnType
+type TUDClickEvent func(sender IObject, button int32)
 
-// TListView TTItemChange
-type TLVChangeEvent func(Sender IObject, Item IObject, Change int32)
+// TLVChangeEvent TListView TTItemChange
+type TLVChangeEvent func(sender IObject, item IObject, change int32)
 
-// Form
-type TCloseEvent func(Sender IObject, Action uintptr)        // Action *uintptr
-type TCloseQueryEvent func(Sender IObject, CanClose uintptr) //CanClose *uintptr
+// TCloseEvent Form
+type TCloseEvent func(sender IObject, action uintptr) // Action *uintptr
+// TCloseQueryEvent Form
+type TCloseQueryEvent func(sender IObject, canClose uintptr) //CanClose *uintptr
 
-// Menu
-type TMenuChangeEvent func(Sender IObject, Source IObject, Rebuild bool)
+// TMenuChangeEvent Menu
+type TMenuChangeEvent func(sender IObject, source IObject, rebuild bool)
 
-// TTreeView
-type TTVChangedEvent func(Sender IObject, Node IObject)
+// TTVChangedEvent TTreeView
+type TTVChangedEvent func(sender IObject, node IObject)
 
-// TLinkLabel
-type TSysLinkEvent = func(Sender IObject, Link string, LinkType int32) // TSysLinkType
+// TSysLinkEvent LinkLabel
+type TSysLinkEvent = func(sender IObject, link string, linkType int32) // TSysLinkType
 
-// TApplication
-type TExceptionEvent = func(Sender, E IObject)
+// TExceptionEvent TApplication
+type TExceptionEvent = func(sender, e IObject)
 
-// procedure(Sender: TObject; var Key: Word; Shift: TShiftState)
-type TKeyEvent = func(Sender IObject, Key uintptr, Shift int32)
+// TKeyEvent = procedure(Sender: TObject; var Key: Word; Shift: TShiftState)
+type TKeyEvent = func(sender IObject, key uintptr, shift int32)
 
 // TKeyPressEvent = procedure(Sender: TObject; var Key: Char) of object;
-type TKeyPressEvent = func(Sender IObject, Key uintptr)
+type TKeyPressEvent = func(sender IObject, key uintptr)
 
 // TMouseEvent = procedure(Sender: TObject; Button: TMouseButton;
 //     Shift: TShiftState; X, Y: Integer) of object;
-type TMouseEvent = func(Sender IObject, Button, Shift, X, Y int32)
+type TMouseEvent = func(sender IObject, button, shift, x, y int32)
 
 // TMouseMoveEvent = procedure(Sender: TObject; Shift: TShiftState;
 //    X, Y: Integer) of object;
-type TMouseMoveEvent = func(Sender IObject, Shift, X, Y int32)
+type TMouseMoveEvent = func(sender IObject, shift, x, y int32)
 
 // TMouseWheelEvent = procedure(Sender: TObject; Shift: TShiftState;
 //    WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean) of object;
-type TMouseWheelEvent = func(Sender IObject, Shift, WheelDelta, X, Y int32, Handled uintptr)
+type TMouseWheelEvent = func(sender IObject, shift, wheelDelta, x, y int32, handled uintptr)

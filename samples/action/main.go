d24eda72b2dc93d9df5034922d67504438f80e95
ying32 <ying_33@live.cn>
2017-10-01 01:18:52 +0800

    添加一个Action

---
+++ samples/action/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"gitee.com/ying32/govcl/vcl"
+	"gitee.com/ying32/govcl/vcl/rtl"
+	"gitee.com/ying32/govcl/vcl/types"
+)
+
+func main() {
+	icon := vcl.NewIcon()
+	defer icon.Free()
+	icon.LoadFromResourceID(rtl.MainInstance(), 3)
+
+	vcl.Application.Initialize()
+	vcl.Application.SetMainFormOnTaskBar(true)
+	vcl.Application.SetIcon(icon)
+
+	mainForm := vcl.Application.CreateForm()
+	mainForm.SetCaption("Hello")
+	mainForm.SetPosition(types.PoScreenCenter)
+	mainForm.EnabledMaximize(false)
+	mainForm.SetWidth(300)
+	mainForm.SetHeight(200)
+	mainForm.SetOnCloseQuery(func(Sender vcl.IObject, CanClose uintptr) {
+		rtl.SetFormCanClose(CanClose, vcl.MessageDlg("是否退出？", types.MtConfirmation, types.MbYes, types.MbNo) == types.IdYes)
+	})
+
+	vcl.Application.Run()
+}

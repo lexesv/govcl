6d8129a04f2c2837d83fde03fe68e37aff592d87
ying32 <ying_33@live.cn>
2017-10-01 01:49:39 +0800

    添加action演示

--- samples/action/main.go
+++ samples/action/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+
 	icon := vcl.NewIcon()
 	defer icon.Free()
 	icon.LoadFromResourceID(rtl.MainInstance(), 3)
@@ -21,9 +22,67 @@ func main() {
 	mainForm.EnabledMaximize(false)
 	mainForm.SetWidth(300)
 	mainForm.SetHeight(200)
-	mainForm.SetOnCloseQuery(func(Sender vcl.IObject, CanClose uintptr) {
-		rtl.SetFormCanClose(CanClose, vcl.MessageDlg("是否退出？", types.MtConfirmation, types.MbYes, types.MbNo) == types.IdYes)
+	// 全局设置提示
+	mainForm.SetShowHint(true)
+
+	imgList := vcl.NewImageList(mainForm)
+	imgList.AddIcon(icon)
+
+	actList := vcl.NewActionList(mainForm)
+	actList.SetImages(imgList)
+
+	// 顶部工具条
+	tlbar := vcl.NewToolBar(mainForm)
+	tlbar.SetParent(mainForm)
+	tlbar.SetImages(imgList)
+
+	tlbtn := vcl.NewToolButton(mainForm)
+	tlbtn.SetParent(tlbar)
+
+	// 底部状态条
+	stbar := vcl.NewStatusBar(mainForm)
+	stbar.SetParent(mainForm)
+	stbar.SetAutoHint(true)
+	stbar.SetSimplePanel(true)
+
+	btn := vcl.NewButton(mainForm)
+	btn.SetParent(mainForm)
+	btn.SetLeft(80)
+	btn.SetTop(40)
+	btn.SetImages(imgList)
+
+	chk := vcl.NewCheckBox(mainForm)
+	chk.SetParent(mainForm)
+	chk.SetCaption("action状态演示")
+	chk.SetLeft(btn.Left())
+	chk.SetTop(btn.Top() - chk.Height() - 10)
+	chk.SetChecked(true)
+
+	// action
+	act := vcl.NewAction(mainForm)
+	act.SetCaption("action")
+	act.SetImageIndex(0)
+	act.SetHint("这是一个提示|长提示了")
+
+	act.SetOnExecute(func(vcl.IObject) {
+		vcl.ShowMessage("点击了action")
 	})
+	act.SetOnUpdate(func(sender vcl.IObject) {
+		vcl.ActionFromObj(sender).SetEnabled(chk.Checked())
+	})
+
+	mainMenu := vcl.NewMainMenu(mainForm)
+	mainMenu.SetImages(imgList)
+
+	menu := vcl.NewMenuItem(mainForm)
+	menu.SetCaption("菜单")
+	mainMenu.Items().Add(menu)
+	subMenu := vcl.NewMenuItem(mainForm)
+	subMenu.SetAction(act)
+	menu.Add(subMenu)
+
+	btn.SetAction(act)
+	tlbtn.SetAction(act)
 
 	vcl.Application.Run()
 }

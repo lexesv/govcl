0852da6a5a97189b438465e07c208b1416b08296
ying32 <ying_33@live.cn>
2017-10-01 03:13:08 +0800

    增加NewMemoryStreamFromBytes

--- vcl/memorystreamdef.go
+++ vcl/memorystreamdef.go
@@ -4,6 +4,13 @@ import (
 	. "gitee.com/ying32/govcl/vcl/api"
 )
 
+// NewMemoryStreamFromBytes 新建Delphi内存流来自Go字节数组
+func NewMemoryStreamFromBytes(data []byte) *TMemoryStream {
+	m := NewMemoryStream()
+	m.Write(data)
+	return m
+}
+
 func (m *TMemoryStream) Read(count int32) (int32, []byte) {
 	return MemoryStream_Read(m.instance, count)
 }

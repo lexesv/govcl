51e8aa1c012ebe70188dddab1c9e88ab545940f6
ying32 <ying_33@live.cn>
2017-10-11 22:09:53 +0800

    更新xui测试例程

--- samples/basicxui/main.go
+++ samples/basicxui/main.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 
 	"gitee.com/ying32/govcl/vcl"
-	//	"gitee.com/ying32/govcl/vcl/api"
 	"gitee.com/ying32/govcl/vcl/rtl"
 	"gitee.com/ying32/govcl/vcl/xui"
 )
 
 const uiXML = `<?xml encoding="utf-8" version="1.0" ?>
-<Form name="mainForm" width="600" height="400" center="true" caption="这是一个测试" enabledmax="false">
+<Form width="800" height="600" center="true" caption="这是一个测试" enabledmax="true">
 	<MainMenu>
 		<MenuItem name="mn1" caption="文件">
 			<MenuItem name="mn3" caption="新建" />
@@ -19,23 +18,38 @@ const uiXML = `<?xml encoding="utf-8" version="1.0" ?>
 		</MenuItem>
 		<MenuItem name="mn2" caption="关于" />
 	</MainMenu>
-	<Button name="btn1" left="10" top="10" caption="按钮" align="alTop"></Button>
-	<Panel name="pnl1" align="alClient">
-		<Button name="btn2" left="10" top="10" caption="消息" onclick="OnButtonClick"></Button>
+	<Button name="Btn1" caption="按钮" align="alTop" />
+	<Panel name="Pnl1" align="alClient">
+		<PageControl name="PgcMain" activeindex="0" align="alClient">
+			<TabSheet caption="第一页">
+			   <Button name="Btn2" left="10" top="10" caption="消息2" onclick="OnButtonClick" />
+			</TabSheet>
+			<TabSheet caption="第二页"> 
+			    <Button name="Btn4" left="100" top="50" caption="消息4" onclick="OnButtonClick" />
+			    <Button name="Btn3" caption="消息3" onclick="OnButtonClick" />
+			</TabSheet>
+		</PageControl>
 	</Panel>
 </Form>`
 
 type TEvents struct {
-	form *vcl.TForm
+	xui.TEvents
+	Btn1    *vcl.TButton
+	Btn2    *vcl.TButton
+	Pnl1    *vcl.TPanel
+	Btn3    *vcl.TButton
+	PgcMain *vcl.TPageControl
 }
 
 func (e *TEvents) OnButtonClick(sender vcl.IObject) {
 	fmt.Println("OnButtonClick.")
+	e.Btn1.SetCaption("changed")
 	vcl.ShowMessage("ButtonClick")
 }
 
 func (e *TEvents) OnMenuExit(sender vcl.IObject) {
-	e.form.Close()
+	fmt.Println("e.Form:", e.Form)
+	e.Form.Close()
 }
 
 func main() {
@@ -49,11 +63,13 @@ func main() {
 
 	events := new(TEvents)
 	m, err := xui.NewFormBytes([]byte(uiXML), events)
-	if err == nil {
-		events.form = m.Form
-	} else {
+	if err != nil {
 		return
 	}
+	fmt.Println("m:", m)
+	fmt.Println("m.btn1", events.Btn3.Left(), events.Btn3.Top(),
+		events.Btn3.Height(), events.Btn3.Width(),
+		events.Btn3.Visible(), events.Btn3.Parent().ClassName())
 
 	vcl.Application.Run()
 }

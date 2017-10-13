b6e9bc181a0fe146d987baa4cf832368496d7425
ying32 <zying32@163.com>
2017-10-13 11:59:21 +0800

    修改DStrToGoStr、GoStrToDStr增加为空判断

--- vcl/api/stdstr_windows.go
+++ vcl/api/stdstr_windows.go
@@ -6,11 +6,18 @@ import (
 )
 
 func DStrToGoStr(ustr uintptr) string {
-	str := make([]uint16, DStrLen(ustr))
+	l := DStrLen(ustr)
+	if l == 0 {
+		return ""
+	}
+	str := make([]uint16, l)
 	DMove(ustr, uintptr(unsafe.Pointer(&str[0])), len(str)*2)
 	return syscall.UTF16ToString(str)
 }
 
 func GoStrToDStr(s string) uintptr {
+	if s == "" {
+		return 0
+	}
 	return uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(s)))
 }

03169ea0ab6de0f4d859db6cbb7debda4f5eda90
ying32 <ying_33@live.cn>
2017-12-22 20:14:33 +0800

    Update samples/basicResForm

---
+++ samples/basicResForm/Form1.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "gitee.com/ying32/govcl/vcl"
+)
+
+type TForm1 struct {
+    *vcl.TForm
+    Button1      *vcl.TButton
+    ComboBox1    *vcl.TComboBox
+    ListBox1     *vcl.TListBox
+    CheckBox1    *vcl.TCheckBox
+    Edit1        *vcl.TEdit
+    Memo1        *vcl.TMemo
+    Button2      *vcl.TButton
+    TreeView1    *vcl.TTreeView
+    ListView1    *vcl.TListView
+    StatusBar1   *vcl.TStatusBar
+    PageControl1 *vcl.TPageControl
+    Panel1       *vcl.TPanel
+    Button3      *vcl.TButton
+    Button4      *vcl.TButton
+    MainMenu1    *vcl.TMainMenu
+    MenuItem13   *vcl.TMenuItem
+    MenuItem14   *vcl.TMenuItem
+    MenuItem15   *vcl.TMenuItem
+    MenuItem16   *vcl.TMenuItem
+    MenuItem26   *vcl.TMenuItem
+    MenuItem25   *vcl.TMenuItem
+    MenuItem27   *vcl.TMenuItem
+    MenuItem28   *vcl.TMenuItem
+    MenuItem29   *vcl.TMenuItem
+    MenuItem17   *vcl.TMenuItem
+    MenuItem18   *vcl.TMenuItem
+    MenuItem30   *vcl.TMenuItem
+    MenuItem31   *vcl.TMenuItem
+    MenuItem33   *vcl.TMenuItem
+    MenuItem35   *vcl.TMenuItem
+    MenuItem36   *vcl.TMenuItem
+    MenuItem37   *vcl.TMenuItem
+    MenuItem38   *vcl.TMenuItem
+    MenuItem39   *vcl.TMenuItem
+    MenuItem19   *vcl.TMenuItem
+    MenuItem20   *vcl.TMenuItem
+    MenuItem21   *vcl.TMenuItem
+    MenuItem23   *vcl.TMenuItem
+    MenuItem24   *vcl.TMenuItem
+    MenuItem22   *vcl.TMenuItem
+    MenuItem32   *vcl.TMenuItem
+    MenuItem34   *vcl.TMenuItem
+    OpenDialog1  *vcl.TOpenDialog
+    ActionList1  *vcl.TActionList
+    ImageList1   *vcl.TImageList
+    PopupMenu1   *vcl.TPopupMenu
+    MenuItem1    *vcl.TMenuItem
+    MenuItem2    *vcl.TMenuItem
+    MenuItem10   *vcl.TMenuItem
+    MenuItem11   *vcl.TMenuItem
+    MenuItem12   *vcl.TMenuItem
+    MenuItem3    *vcl.TMenuItem
+    MenuItem4    *vcl.TMenuItem
+    MenuItem5    *vcl.TMenuItem
+    MenuItem6    *vcl.TMenuItem
+    MenuItem7    *vcl.TMenuItem
+    MenuItem8    *vcl.TMenuItem
+    MenuItem9    *vcl.TMenuItem
+    Button5      *vcl.TButton
+}
+
+var Form1 *TForm1
+
+
+
+
+// vcl.Application.CreateFormFromFile("Form1.gfm", &Form1)
--- samples/basicResForm/main.go
+++ samples/basicResForm/main.go
@@ -7,17 +7,6 @@ import (
 	"gitee.com/ying32/govcl/vcl/rtl"
 )
 
-// 类似一个继承的，用于自动填充字段
-type TMainForm struct {
-	*vcl.TForm
-	Button1   *vcl.TButton
-	CheckBox1 *vcl.TCheckBox
-	ComboBox1 *vcl.TComboBox
-	ListBox1  *vcl.TListBox
-}
-
-var mainForm *TMainForm
-
 func main() {
 
 	icon := vcl.NewIcon()
@@ -28,18 +17,18 @@ func main() {
 	vcl.Application.SetMainFormOnTaskBar(true)
 	vcl.Application.SetIcon(icon)
 
-	vcl.Application.CreateFormFromFile("./Form1.gfm", &mainForm)
-	fmt.Println(mainForm.Button1)
-	mainForm.Button1.SetOnClick(func(sender vcl.IObject) {
+	vcl.Application.CreateFormFromFile("Form1.gfm", &Form1)
+	fmt.Println(Form1.Button1)
+	Form1.Button1.SetOnClick(func(sender vcl.IObject) {
 		vcl.ShowMessage("Hello!")
 	})
 
-	mainForm.CheckBox1.SetOnClick(func(sender vcl.IObject) {
-		mainForm.Button1.SetEnabled(mainForm.CheckBox1.Checked())
-		vcl.ShowMessage(mainForm.ComboBox1.Text())
+	Form1.CheckBox1.SetOnClick(func(sender vcl.IObject) {
+		Form1.Button1.SetEnabled(Form1.CheckBox1.Checked())
+		vcl.ShowMessage(Form1.ComboBox1.Text())
 	})
 
-	mainForm.SetOnCloseQuery(func(sender vcl.IObject, canClose *bool) {
+	Form1.SetOnCloseQuery(func(sender vcl.IObject, canClose *bool) {
 		fmt.Println("关闭。")
 	})
 

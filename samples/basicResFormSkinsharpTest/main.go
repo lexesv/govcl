8b05445ef1ff5e3318d6b882b41f72282cb6d6fb
ying32 <ying_33@live.cn>
2018-01-05 10:57:59 +0800

    修复包导入错误

--- samples/basicResFormSkinsharpTest/main.go
+++ samples/basicResFormSkinsharpTest/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"gitee.com/ying32/golcl/lcl/types"
 	"gitee.com/ying32/govcl/vcl"
 	"gitee.com/ying32/govcl/vcl/exts/skinh"
+	"gitee.com/ying32/govcl/vcl/types"
 )
 
 func main() {

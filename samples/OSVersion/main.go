2032a0c1ca4e4756885bd5c6770df75958812939
ying32 <ying_33@live.cn>
2018-05-03 03:15:37 +0800

    Windows版本信息使用

--- samples/OSVersion/main.go
+++ samples/OSVersion/main.go
@@ -7,6 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Println(rtl.OSVersion)
+	//fmt.Println(rtl.OSVersion)
+	fmt.Println(rtl.OSVersion.Major)
+	fmt.Println(rtl.OSVersion.Minor)
 	fmt.Println(rtl.OSVersion.Name)
+	fmt.Println(rtl.OSVersion.ToString())
+	if rtl.OSVersion.Check1(10) {
+		fmt.Println("当前为Windows10")
+	} else {
+		fmt.Println("当前不是Windows10")
+	}
 }

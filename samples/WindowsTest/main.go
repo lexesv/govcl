18cc36d69f760d93a48b095843e64d3ab8d885e2
ying32 <ying_33@live.cn>
2018-05-05 22:21:16 +0800

    更新WindowsTest例程

--- samples/WindowsTest/main.go
+++ samples/WindowsTest/main.go
@@ -8,6 +8,24 @@ import (
 
 func main() {
 
+	// 利用互斥来演示exe单一运行，当然不止这一种方法了
+	// GetLastError 无法获取错误，待研究原因所在。
+	/*
+		Mutex := win.CreateMutex(nil, true, "SingleRunTest")
+		defer win.ReleaseMutex(Mutex)
+		fmt.Println("Mutex:", Mutex)
+		fmt.Println("win.GetLastError:", win.GetLastError(), syscall.GetLastError())
+		if win.GetLastError() == win.ERROR_ALREADY_EXISTS {
+			win.MessageBox(0, "我已经在运行中啦！", "运行提示", win.MB_OK+win.MB_ICONINFORMATION)
+			os.Exit(1)
+		}
+
+		Mutex3 := win.CreateMutex(nil, true, "SingleRunTest")
+
+		fmt.Println("win.GetLastError:", win.GetLastError())
+		defer win.ReleaseMutex(Mutex3)
+	*/
+
 	// Exe运行在Administrator权限下检测
 	fmt.Println("IsAdministrator:", win.IsAdministrator())
 

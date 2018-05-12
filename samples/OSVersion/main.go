e520f8707844cafe07e161823a54acae0db72433
ying32 <ying_33@live.cn>
2018-05-12 15:04:22 +0800

    添加Mac下 OSVersion信息

--- samples/OSVersion/main.go
+++ samples/OSVersion/main.go
@@ -7,14 +7,22 @@ import (
 )
 
 func main() {
-	//fmt.Println(rtl.OSVersion)
-	fmt.Println(rtl.OSVersion.Major)
-	fmt.Println(rtl.OSVersion.Minor)
-	fmt.Println(rtl.OSVersion.Name)
-	fmt.Println(rtl.OSVersion.ToString())
-	if rtl.OSVersion.CheckMajor(10) {
-		fmt.Println("当前为Windows10")
-	} else {
-		fmt.Println("当前不是Windows10")
+
+	fmt.Println("Major: ", rtl.OSVersion.Major)
+	fmt.Println("Minor: ", rtl.OSVersion.Minor)
+	fmt.Println("Name: ", rtl.OSVersion.Name)
+	fmt.Println("String: ", rtl.OSVersion.ToString())
+	switch rtl.OSVersion.Platform {
+	case rtl.PfWindows:
+		if rtl.OSVersion.CheckMajor(10) {
+			fmt.Println("当前为Windows10")
+		} else {
+			fmt.Println("当前不是Windows10")
+		}
+	case rtl.PfLinux:
+		fmt.Println("LIBC Version:", rtl.OSVersion.LibCVersionMajor, ", ", rtl.OSVersion.LibCVersionMinor)
+	case rtl.PfMacOS:
+		//
 	}
+
 }
